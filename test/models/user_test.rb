2cbdac505f47448a9e9e8fcebf21cf001db5f3b3
tabatalisboa <tabatalisboa@protonmail.com>
2020-11-16 02:25:47 -0300

    Add 1st validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,10 +2,20 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email:"user@example.com", cpf:1176784579)
+    @user = User.new(name: "Example User", email: "user@example.com", cpf:1176784579)
   end
 
   test "should be valid" do
     assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
   end
 end
